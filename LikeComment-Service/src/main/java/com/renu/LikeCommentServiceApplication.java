b656fa5babb1b4915529340dfc2f9192deacf309
Atif Aslam <aslam.renu.soft43@gmail.com>
2019-01-22 14:29:11 -0800

    Update completed

---
+++ LikeComment-Service/src/main/java/com/renu/LikeCommentServiceApplication.java
@@ -0,0 +1,14 @@
+package com.renu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeCommentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikeCommentServiceApplication.class, args);
+	}
+
+}
+
---
+++ NotificationManagement-Service/src/main/java/com/renu/NotificationManagementServiceApplication.java
@@ -0,0 +1,14 @@
+package com.renu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationManagementServiceApplication.class, args);
+	}
+
+}
+
