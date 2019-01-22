4ffc3303d91813ef213f1f231706b93ed62b129a
Atif Aslam <aslam.renu.soft43@gmail.com>
2019-01-22 14:42:13 -0800

    Update completed

--- ProfileStock-Service/src/main/java/com/renu/ProfileStockServiceApplication.java
+++ ProfileStock-Service/src/main/java/com/renu/ProfileStockServiceApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
-public class AccountStockServiceApplication {
+public class ProfileStockServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AccountStockServiceApplication.class, args);
+		SpringApplication.run(ProfileStockServiceApplication.class, args);
 	}
 
 }
--- ProfileStock-Service/src/test/java/com/renu/ProfileStockServiceApplicationTests.java
+++ ProfileStock-Service/src/test/java/com/renu/ProfileStockServiceApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class AccountStockServiceApplicationTests {
+public class ProfileStockServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
